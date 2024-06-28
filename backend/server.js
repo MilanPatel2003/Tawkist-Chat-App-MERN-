be6305a7e0355f95daf4068be977608f1b3586d1
MilanPatel2003 <milanpatel6454@gmail.com>
2024-06-29 04:41:42 +0530

    render deployment code added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,19 +9,25 @@ import userRoutes from "./routes/user.routes.js";
 import dbConnection from "./db/dbConnection.js";
 import { server, io, app } from "./socket/Socket.js";
 // import errorMiddleware from "./middlewares/errorMiddleware.js";
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(cookieParser());
 app.use(express.json()); // parse json data (from req.body)
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/user", userRoutes);
 // app.use(errorMiddleware)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   dbConnection();
   console.log(`Server is running on port ${PORT}`);

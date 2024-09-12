c4c15d4de712e87badb77d79a5cbb4d04c8c96cb
milanpatel01 <milan.patel@codefencers.com>
2024-09-12 22:37:54 +0530

    kash theme

--- frontend/src/components/themecontroller/themeController.jsx
+++ frontend/src/components/themecontroller/themeController.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ThemeController = () => {
-  const [theme, setTheme] = useState('default');
+  const [theme, setTheme] = useState("default");
 
   const handleThemeChange = (event) => {
     setTheme(event.target.value);
@@ -10,34 +10,84 @@ const ThemeController = () => {
 
   return (
     <div>
-      <div className="form-control" style={{ position: 'fixed', top: '10px', right: '10px' }}>
+      <div
+        className="form-control"
+        style={{ position: "fixed", top: "10px", right: "10px" }}
+      >
         <label className="label cursor-pointer gap-4">
           <span className="label-text text-white">Default</span>
-          <input type="radio" name="theme-radios" className="radio  theme-controller" value="default" checked={theme === 'default'} onChange={handleThemeChange} />
+          <input
+            type="radio"
+            name="theme-radios"
+            className="radio  theme-controller"
+            value="default"
+            checked={theme === "default"}
+            onChange={handleThemeChange}
+          />
         </label>
       </div>
-      <div className="form-control" style={{ position: 'fixed', top: '40px', right: '10px' }}>
+      <div
+        className="form-control"
+        style={{ position: "fixed", top: "40px", right: "10px" }}
+      >
         <label className="label cursor-pointer gap-4">
           <span className="label-text text-white">Your Name</span>
-          <input type="radio" name="theme-radios" className="radio  theme-controller" value="yourname" checked={theme === 'yourname'} onChange={handleThemeChange} />
+          <input
+            type="radio"
+            name="theme-radios"
+            className="radio  theme-controller"
+            value="yourname"
+            checked={theme === "yourname"}
+            onChange={handleThemeChange}
+          />
         </label>
       </div>
-      <div className="form-control" style={{ position: 'fixed', top: '70px', right: '10px' }}>
+      <div
+        className="form-control"
+        style={{ position: "fixed", top: "70px", right: "10px" }}
+      >
         <label className="label cursor-pointer gap-4">
           <span className="label-text text-white">Jujutsu-Kaisen</span>
-          <input type="radio" name="theme-radios" className="radio  theme-controller" value="jujutsukaisen" checked={theme === 'jujutsukaisen'} onChange={handleThemeChange} />
+          <input
+            type="radio"
+            name="theme-radios"
+            className="radio  theme-controller"
+            value="jujutsukaisen"
+            checked={theme === "jujutsukaisen"}
+            onChange={handleThemeChange}
+          />
         </label>
       </div>
-      <div className="form-control" style={{ position: 'fixed', top: '100px', right: '10px' }}>
+      <div
+        className="form-control"
+        style={{ position: "fixed", top: "100px", right: "10px" }}
+      >
         <label className="label cursor-pointer gap-4">
           <span className="label-text text-white">Avengers</span>
-          <input type="radio" name="theme-radios" className="radio  theme-controller" value="avengers" checked={theme === 'avengers'} onChange={handleThemeChange} />
+          <input
+            type="radio"
+            name="theme-radios"
+            className="radio  theme-controller"
+            value="avengers"
+            checked={theme === "avengers"}
+            onChange={handleThemeChange}
+          />
         </label>
       </div>
-      <div className="form-control" style={{ position: 'fixed', top: '130px', right: '10px' }}>
+      <div
+        className="form-control"
+        style={{ position: "fixed", top: "130px", right: "10px" }}
+      >
         <label className="label cursor-pointer gap-4">
-          <span className="label-text text-white">Waifu</span>
-          <input type="radio" name="theme-radios" className="radio  theme-controller" value="waifu" checked={theme === 'waifu'} onChange={handleThemeChange} />
+          <span className="label-text text-white">🎀Kash ki theme🎀</span>
+          <input
+            type="radio"
+            name="theme-radios"
+            className="radio  theme-controller"
+            value="waifu"
+            checked={theme === "waifu"}
+            onChange={handleThemeChange}
+          />
         </label>
       </div>
     </div>
